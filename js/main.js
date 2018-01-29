338af4a51cf663bf160150c53058b2e86d4de772
Jeferson P <jeferson_p.silva@outlook.com>
2018-01-29 09:13:31 -0200

    Alterações no estilo do header, correção em bug de posicionamneto

--- js/main.js
+++ js/main.js
@@ -8,11 +8,5 @@ $(document).ready(function() {
 	    }, 1000);
 	    return false;
 	});
-
-		/*
-			<blockquote class="blockquote">
-		  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-		  <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-		</blockquote>
-		*/
+	
 });
