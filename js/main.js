a2d5b0e898961bb85ea0eb13418129613da2702c
Jeferson P <jeferson_p.silva@outlook.com>
2018-02-01 11:46:44 -0200

    Adicionado estrutura e informações na área de conhecimentos, usando panel collapse

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 	//Tecnica para deslizar ao clicar no href :target com coldown de 1s
 	var $doc = $("html, body");
 	$(".scrollSuave").click(function() {
@@ -9,4 +8,25 @@ $(document).ready(function() {
 	    return false;
 	});
 	
+
+	$('[id^=detail-]').hide();
+    $('.toggle').click(function() {
+        $input = $( this );
+        $target = $('#'+$input.attr('data-toggle'));
+        $target.slideToggle();
+    });
+	
+});
+
+$(document).on("click", ".panel-heading span.pressionado", function(e){
+    var $this = $(this);
+	if(!$this.hasClass("panel-collapsed")) {
+		$this.parents(".panel").find(".panel-body").slideUp();
+		$this.addClass("panel-collapsed");
+		$this.find("i").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+	} else {
+		$this.parents(".panel").find(".panel-body").slideDown();
+		$this.removeClass("panel-collapsed");
+		$this.find("i").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+	}
 });
