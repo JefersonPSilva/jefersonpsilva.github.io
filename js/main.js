2a4bb1c61e5ec28010e2681d9e2c74d2f607c5fe
Jeferson P <jeferson_p.silva@outlook.com>
2018-02-06 11:56:54 -0200

    Adiconado mask no form da área de contato com jquery mask plugin

--- js/main.js
+++ js/main.js
@@ -9,42 +9,51 @@ $(document).ready(function() {
 	});
 	
 	// Evento drop down área conhecimento, itens especificos
-	$('[id^=detail-]').hide();
-    $('.toggle').click(function() {
+	$("[id^=detail-]").hide();
+    $(".toggle").click(function() {
         $input = $( this );
-        $target = $('#'+$input.attr('data-toggle'));
+        $target = $("#"+$input.attr("data-toggle"));
         $target.slideToggle();
     });
 
     // Validar form e-mail
-    $('#btnenviar').click(function(e){   
-        var inNome = $('#txtinputnome'),
-        	inEmail = $('#txtinputemail'),
-        	inAssunto = $('#txtinputassunto'),
-        	inArea = $('#txtinputarea'),
-        	btnEnviar = $('#btnenviar');
+    $("#btnenviar").click(function(e){   
+        var inNome = $("#txtinputnome"),
+        	inEmail = $("#txtinputemail"),
+        	inAssunto = $("#txtinputassunto"),
+        	inArea = $("#txtinputarea"),
+        	btnEnviar = $("#btnenviar");
         
         var emailformat = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        var latinformat=/[^a-zA-Z\-\'\ ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏàáâãäåæçèéêëìíîïÐÑÒÓÔÕÖØÙÚÛÜÝÞßðñòóôõöøùúûüýþÿ]/i;
+        var latinformat= /[^a-zA-Z\-\'\ ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏàáâãäåæçèéêëìíîïÐÑÒÓÔÕÖØÙÚÛÜÝÞßðñòóôõöøùúûüýþÿ]/i;
         
         if($inNome.val().length<=2 || latinformat.test($inNome.val())){
             e.preventDefault();
-            $('#txtinputnome + span').toggle().html("Informe um nome válido !");
+            $("#txtinputnome + span").toggle().html("Informe um nome válido !");
             $inNome.focus();                 
         }else if(!emailformat.test($inEmail.val())){
             e.preventDefault();
-            $('#txtinputemail + span').toggle().html("Informe um e-mail válido !");
+            $("#txtinputemail + span").toggle().html("Informe um e-mail válido !");
             $inEmail.focus();                 
         }else if($inAssunto.val().length<=2 || !latinformat.test($inAssunto.val())){
             e.preventDefault();
-            $('#txtinputassunto + span').toggle().html("Informe um assunto válido !");
+            $("#txtinputassunto + span").toggle().html("Informe um assunto válido !");
             $inAssunto.focus();                 
         }else if($inArea.val().length<=2 || !latinformat.test($inArea.val())){
             e.preventDefault();
-            $('#txtinputarea + span').toggle().html("Informe uma mensagem válida !");
+            $("#txtinputarea + span").toggle().html("Informe uma mensagem válida !");
             $inArea.focus();                 
         }
     });
+
+    //jquery mask plugin configuração form contato
+    $("#telefone").mask("(00) 00000-0000", {placeholder: "(00) 00000-0000"});
+    $("#email").mask("A", {
+		translation: {
+			"A": { pattern: /[\w@\-.+]/, recursive: true }
+		},
+		placeholder: "contato@contato.com"
+	});
 });
 
 //Evento drop down área conhecimentos
