51442ed32b5e1fb42146d8df34a8639ae81b29ba
JefersonPSilva <jeferson_p.silva@outlook.com>
2018-02-12 17:04:13 -0200

    Finalizado validação e mask dos campos form contato

--- js/validacao.js
+++ js/validacao.js
@@ -1,62 +1,73 @@
+//https://github.com/igorescobar/jQuery-Mask-Plugin
 //jquery mask plugin configuração mask form
-$("#telefone").mask("(00) 00000-0000", {placeholder: "(00) 00000-0000"});
-$("#email").mask("A", {
-    translation: {
-        "A": { pattern: /[\w@\-.+]/, recursive: true }
-    },
-    placeholder: "contato@contato.com"
-});,
+$(document).ready(function() {
+    $("#telefone").mask("(00) 00000-0000", {placeholder: "(00) 00000-0000"});
 
-// Métodos para validação com nono dígito
-jQuery.validator.addMethod("digit9", function(value, element, param) {
-    return this.optional(element) || /^9[0-9]{9}$/.test(value);
-},"Requer o digito 9.");
+    (function(){
+        emailjs.init("user_nPcjen9FAB5mMoXpExSKf");
+    })();
+});
 
-// Métodos para validação sem nono dígito
-jQuery.validator.addMethod("tel", function(value, element, param) {
-    return this.optional(element) || /^[0-9]{9}$/.test(value);
-},"Digite seu numero.");
 
-$("#form").validate({
-    errorContainer: "#erros",
-    errorLabelContainer: "#erros",
-    wrapper: "li",
-    rules: {
-        ddd: {
-            required: true,
-            number: true,
-            maxlength: 2,
-            minlength: 2
-        },
-        phone: {
-            required: true,
-            number: true,
-            digit9: {
-                depends: function(element){
-                   return ($('.ddd').val() == 11);
-                }
+$(function(){
+    //https://jqueryvalidation.org
+    //jquery validation plugin
+    jQuery.validator.addMethod("phoneBR", function(value, element) {
+        return this.optional( element ) || /^(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/.test(value);
+    }, "Este número de celular e inválido.");
+
+    $("#formContato").validate({
+        rules:{
+            email: {
+                required: true,
+                email: true
             },
-            tel: {
-                depends: function(element){
-                    return ($('.ddd').val() != 11);
-                }
+            nome: {
+                required: true,
+                minlength: 2
+            },
+            assunto: {
+                required: true,
+                minlength: 2,
+            },
+            telefone:{
+                phoneBR: true
+            },
+            mensagem: {
+                required: true
             }
-        }
-    },
-    messages: {
-        ddd: {
-            required: "O campo DDD é obrigatorio.",
-            number: "O campo DDD aceita apenas digitos numericos.",
-            maxlength: "O campo DDD requer apenas dois digitos.",
-            minlength: "O campo DDD requer dois digitos."
         },
-        phone: {
-            required: "O campo telefone é obrigatorio.",
-            number: "O campo telefone aceita apenas digitos numericos."
+        messages: {
+            email: {
+                required: "Por favor, informe um email.",
+                email: "Este email e inválido."
+            },
+            nome: {
+                required: "Por favor, informe seu nome.",
+                minlength: "Por favor, digite pelo menos 2 caracteres."
+            },
+            assunto: {
+                required: "Por favor, informe do que se trata.",
+                minlength: "Por favor, digite pelo menos 2 caracteres."
+            },
+            mensagem: {
+                required: "Por favor, digite sua mensagem."
+            }
+        },
+        submitHandler: function (form) {
+            if (form.valid()) {
+                emailjs.send("user_nPcjen9FAB5mMoXpExSKf","template_FXxnEYmN", {
+                    name: "James", 
+                    notes: "Check this out!"
+                })
+                .then(function(response) {
+                   console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+                }, function(err) {
+                   console.log("FAILED. error=", err);
+                });
+                // $(".sucess_box").css("display", "block");          
+            };
+            return false;
         }
-    },
-    submitHandler: function(form) {
-        alert('success');
-       //form.submit();
-    }
+    });
 });
